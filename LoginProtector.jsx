597ca89cd67ed7fe1d220e4a8fa607e9c0f066c0
SRIDHARA VENKATA SAI NAGA TEJA SWARUP <sridharateja2@gmail.com>
2025-07-07 19:37:38 +0530

    Delete LoginProtector.jsx

--- LoginProtector.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-export default function LoginProtector({ children }) {
-  const token = localStorage.getItem('userToken');
-  return token ? <Navigate to="/profile" replace /> : children;
-}
